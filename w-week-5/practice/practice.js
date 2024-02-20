e2dc268236ae8462e38818e4be1f843df9fba3a4
Ali Hussain <135874844+alikhokar@users.noreply.github.com>
2024-02-20 17:04:36 +0500

    Create practice.js

---
+++ w-week-5/practice/practice.js
@@ -0,0 +1,20 @@
+const arrayToPass = [1,2,3];
+const arrayOfdouble
+console.log(arrayToPass);
+arrayToPass.push(9);
+console.log(arrayToPass);
+arrayToPass.pop();
+console.log(arrayToPass);
+var x= arrayToPass.shift();
+console.log(x);
+console.log(arrayToPass);
+var length=arrayToPass.unshift(1,2);
+console.log("Length of the array after unshifting: "+length);
+console.log(arrayToPass);
+var objName={
+    var obj={};
+    obj['1']=1;
+    console.log(obj);
+}
+
+
