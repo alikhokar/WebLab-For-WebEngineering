91f574e2c5a39207d2b4e5111c603a9ac25db2c6
Ali Hussain <135874844+alikhokar@users.noreply.github.com>
2024-04-02 14:59:31 +0500

    Update index.js

--- Mid-one/index.js
+++ Mid-one/index.js
@@ -1,1 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+  const img = document.querySelectorAll('.simg');
+  const pbtn = document.querySelector('.prev');
+  const nbtn = document.querySelector('.next');
+  const thumb = document.querySelector('.container');
+
+  let cur = 0;
+
+  function showImage(indx) {
+    img.forEach((image, i) => {
+      if (i === indx) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
+    });
+    update(indx);
+  }
+
+  function update(indx) {
+    thumb.innerHTML = '';
+    img.forEach((_, i) => {
+      const thumbnail = document.createElement('img');
+      thumbnail.src = img[i].src;
+      thumbnail.classList.add('thumbnail');
+      if (i === indx) {
+        thumbnail.classList.add('active-thumbnail');
+      }
+      thumbnail.addEventListener('click', () => showImage(i));
+      thumb.appendChild(thumbnail);
+    });
+  }
+
+  pbtn.addEventListener('click', () => {
+    cur = (cur - 1 + img.length) % img.length;
+    showImage(cur);
+  });
+
+  nbtn.addEventListener('click', () => {
+    cur = (cur + 1) % img.length;
+    showImage(cur);
+  });
+
+  showImage(cur);
+});
